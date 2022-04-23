16edc633c60fb52078e7f0afafe83e2434bfb32c
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-04-23 13:32:44 +0530

    a=int(input()) k=0 for i in range (1,a):     if(a==i*i):         k=1 if(k==1):     print("True") else:     print("False")

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,9 +1,9 @@
-n=int(input())
-c=0
-for i in range(1,n//2):
-    if(n==i*i):
-        c=1
-if(c==1):
+a=int(input())
+k=0
+for i in range (1,a):
+    if(a==i*i):
+        k=1
+if(k==1):
     print("True")
 else:
     print("False")
