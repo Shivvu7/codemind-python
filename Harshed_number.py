449e7c8922094ffd975d9ecd33bb332f7fab382a
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-05-24 10:58:33 +0530

    def hars(n):     t=n     s=0     while(n):         d=n%10         s=s+d         n=n//10     if(t%s==0):         return True     else:         return False n=int(input()) if(hars(n)):     print('True') else:     print('False')

--- Harshed_number.py
+++ Harshed_number.py
@@ -1,11 +1,16 @@
-a=int(input())
-s=0
-t=a
-while(a):
-    d=a%10
-    s=s+d
-    a=a//10
-if(t%s==0):
-    print("True")
+def hars(n):
+    t=n
+    s=0
+    while(n):
+        d=n%10
+        s=s+d
+        n=n//10
+    if(t%s==0):
+        return True
+    else:
+        return False
+n=int(input())
+if(hars(n)):
+    print('True')
 else:
-    print("False")
+    print('False')
