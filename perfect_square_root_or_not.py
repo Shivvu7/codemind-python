f2141b57f4037f3f08db1b388dce87584e12fb28
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-06-22 23:28:48 +0530

    n=int(input()) c=0 for i in range(1,int(n**0.5)+1):     if(i==(n**0.5)):         c=1 if(c==1):     print("True") else:     print("False")

--- perfect_square_root_or_not.py
+++ perfect_square_root_or_not.py
@@ -1,9 +1,9 @@
-a=int(input())
-k=0
-for i in range (1,a):
-    if(a==i*i):
-        k=1
-if(k==1):
+n=int(input())
+c=0
+for i in range(1,int(n**0.5)+1):
+    if(i==(n**0.5)):
+        c=1
+if(c==1):
     print("True")
 else:
     print("False")
