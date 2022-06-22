36177ce1d1e4e8b004c0e2618defe2f3b37762cc
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-06-22 23:08:15 +0530

    def no(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return True     else:         return False n=int(input()) c=1 for i in range(1,n+1):     if no(i):         if(n%i==0):             c+=1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,13 +1,13 @@
-def nonprime(n):
+def no(n):
     for i in range(2,int(n**0.5)+1):
-        if(n%i==0):
+        if n%i==0:
             return True
     else:
         return False
 n=int(input())
 c=1
-for j in range(1,n+1):
-    if(n%j==0):
-        if(nonprime(j)):
+for i in range(1,n+1):
+    if no(i):
+        if(n%i==0):
             c+=1
 print(c)
