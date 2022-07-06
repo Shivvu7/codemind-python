515bd10e85db45f41173aa1c7a645831f2da6fa5
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-07-06 11:31:40 +0530

    def fun(n):     for i in range(2,int(n**0.5)+1):         if n%i==0:             return False     else:         return True n=int(input()) a=list(map(int,input().split()[:n])) k=int(input()) c=0 for i in a:     if fun(i) and i>1:         if i<=k:             c+=1 print(c)

---
+++ primes_less_than_k.py
@@ -0,0 +1,15 @@
+def fun(n):
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    else:
+        return True
+n=int(input())
+a=list(map(int,input().split()[:n]))
+k=int(input())
+c=0
+for i in a:
+    if fun(i) and i>1:
+        if i<=k:
+            c+=1
+print(c)
