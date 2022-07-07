746d77cb1e621d9c689e505582c07a0621609cb4
Sariki Sivasai <99276946+Shivvu7@users.noreply.github.com>
2022-07-07 10:36:56 +0530

    def fun(n):     c=0     while(n):         d=n%10         c+=1         n//=10     return c n=int(input()) a=list(map(int,input().split()[:n])) k=[] for i in a:     b=(fun(i))     k.append(b)     c=min(k)     res=k.count(c) print(res)

---
+++ Minimum_digit_elements.py
@@ -0,0 +1,16 @@
+def fun(n):
+    c=0
+    while(n):
+        d=n%10
+        c+=1
+        n//=10
+    return c
+n=int(input())
+a=list(map(int,input().split()[:n]))
+k=[]
+for i in a:
+    b=(fun(i))
+    k.append(b)
+    c=min(k)
+    res=k.count(c)
+print(res)
